ba85328010e4d84b98192e91373a96779dc42055
MonaaEid <monaa_eid@hotmail.com>
2024-06-18 22:47:42 +0300

    Add dirs

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+import { MongoClient } from 'mongodb';
+import Collection from 'mongodb/lib/collection';
+
+
+class DBClient {
+    constructor() {
+
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const dbURL = `mongodb://${host}:${port}/${database}`;
+    
+        this.client = new MongoClient(dbURL, { useUnifiedTopology: true });
+        this.client.connect();
+      }    
+
+    isAlive() {
+        return this.client._ending === false;
+        }
+    async nbUsers() {
+        const res = await this.client.db.Collection('users').countDocuments();
+        return res;
+    }
+
+    async nbFiles() {
+        const res = await this.client.db.Collection('files').countDocuments();
+        return res;
+    }
+}
+
+const dbClient = new DBClient();
+export default dbClient;

ce2bc582abc631defa16eaa8363dcbe11dfc0aac
MonaaEid <monaa_eid@hotmail.com>
2024-06-19 09:20:02 +0300

    Add dirs

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
+    const userExists = await dbClient.usersCollection.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).send({ error: 'Already exist' });
+    }
+
+
+    const user = await dbClient.usersCollection.insertOne({ email, password: sha1(password) });
+
+    return res.status(201).send({ id: user.insertedId, email });
+  }
+}
+
+export default UsersController;

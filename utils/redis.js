d3b65eaf3aed6888607f524a3b2d6b1918f5af1d
MonaaEid <monaa_eid@hotmail.com>
2024-06-20 03:27:27 +0300

    Add Auth

--- utils/redis.js
+++ utils/redis.js
@@ -4,6 +4,9 @@ import promisify from 'util';
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(error);
+    });
   }
 
   isAlive() {
